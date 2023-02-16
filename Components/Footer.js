ced3b2fba81533b7c48f6c6d6ae35aaa97937b61
MichaelShadd <michaelshaddy@hotmail.com>
2023-02-16 12:37:17 -0800

    Added links to twitter and insta icons.

--- Components/Footer.js
+++ Components/Footer.js
@@ -4,9 +4,9 @@ import React from "react"
 export default function Footer () {
     return (
         <footer>
-            <a href="" target="mywindow"><img src="../Images/twittericon.png" /></a>
+            <a href="https://twitter.com/shaddy_michael" target="mywindow"><img src="../Images/twittericon.png" /></a>
             <a href="https://www.facebook.com/michael.shaddy.5" target="mywindow"><img src="../Images/facebookicon.png" /></a>
-            <a href="" target="mywindow"><img src="../Images/instaicon.png" /></a>
+            <a href="https://www.instagram.com/michael_jayyys/" target="mywindow"><img src="../Images/instaicon.png" /></a>
             <a href="https://github.com/michaelshaddy" target="mywindow"><img src="../Images/githubicon.png" /></a>
         </footer>
     )
